486873ee84959b7c2f230bf1f4aace85e3b7c72f
Ricardo Naga <ric.nagato@gmail.com>
2024-08-21 18:00:46 -0300

    Add microfrontend for nextjs

--- packages/remotes/nextjs/next.config.mjs
+++ packages/remotes/nextjs/next.config.mjs
@@ -1,4 +1,21 @@
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "remoteNextjs",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./page": "./src/pages",
+        },
+      })
+    );
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- packages/shell/nextjs/next.config.mjs
+++ packages/shell/nextjs/next.config.mjs
@@ -1,4 +1,29 @@
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
+
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+  return {
+    remoteNextjs: `remoteNextjs@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+  };
+};
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "shellNextjs",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: remotes(isServer),
+        extraOptions: {
+          exposePages: true,
+        },
+      })
+    );
+
+    return config;
+  },
+};
 
 export default nextConfig;

a6a9eb172c526e24f2e2018601ed4bf3e6608db1
Ricardo Naga <ric.nagato@gmail.com>
2024-08-21 15:45:13 -0300

    add remote/shell initial config for react vite/webpack

---
+++ packages/shell/react-vite/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+import { dependencies } from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "shellVite",
+      remotes: {
+        remoteVite: "http://localhost:4001/assets/remoteEntry.js",
+      },
+      shared: Object.keys(dependencies),
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    port: 4000,
+  },
+});

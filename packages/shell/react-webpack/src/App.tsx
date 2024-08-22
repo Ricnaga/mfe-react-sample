dfb58a0c5ef00a31263f4d70b471460f4969704c
Ricardo Naga <ric.nagato@gmail.com>
2024-08-22 18:18:19 -0300

    refacotr(Remote): Update module remote names

--- packages/shell/react-webpack/src/App.tsx
+++ packages/shell/react-webpack/src/App.tsx
@@ -1,7 +1,6 @@
 import { Suspense, lazy } from "react";
 
 const RemoteWebpack = lazy(() => import("@mfe-react/remote-react-webpack/App"));
-const RemoteNextjs = lazy(() => import("@mfe-react/remote-nextjs/page"));
 
 function App() {
   return (
@@ -9,9 +8,6 @@ function App() {
       <h1>THIS IS REACT WEBPACK HOST</h1>
       <Suspense fallback={"loading RemoteWebpack..."}>
         <RemoteWebpack />
-      </Suspense>
-      <Suspense fallback={"loading RemoteNextjs..."}>
-        <RemoteNextjs />
       </Suspense>
     </>
   );
